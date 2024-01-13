c8547f459bf3205008c501ac404765ed2dd263a2
Ahad69 <ahadxx99@gmail.com>
2024-01-13 15:38:14 +0600

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import "@/styles/globals.css";
-import "@/styles/fonts.css";
+//import "@/styles/globals.css";
+//import "@/styles/fonts.css";
+import "../styles/globals.css";
 
 import { useEffect } from "react";
 
